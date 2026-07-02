c742c2be7c2586069b773a3d43630a476c1896e4
AlexPBouds <alexcjboudreau@gmail.com>
2026-07-02 06:19:18 -0300

    Started working on popup to add book

--- script.js
+++ script.js
@@ -22,9 +22,9 @@ function addBookToLibrary(title, author, pages, read) {
 
 function displayAllBooks() {
   const libraryLength = library.length;
-  const libraryDiv = document.getElementsByClassName("library")[0];
+  const libraryDiv = document.getElementsByClassName("libraryContainer")[0];
 
-  // Reset the library before showing all the books
+  // Reset the library and add the Library title
   libraryDiv.innerHTML = "";
 
   // Create the book cards, and add them to the library div element
@@ -33,12 +33,17 @@ function displayAllBooks() {
     const bookCard = document.createElement("div");
     bookCard.classList.add("card");
 
-    const bookTitle = document.createTextNode(`${currentBook.title}`);
+    const bookTitle = document.createElement("p");
+    bookTitle.innerText = `Title: ${currentBook.title}`;
     bookTitle.classList.add("title");
-    const bookAuthor = document.createTextNode(`${currentBook.author}`);
-    const bookPages = document.createTextNode(`${currentBook.pages}`);
-    const bookRead = document.createTextNode(`${currentBook.read}`);
-    const bookCount = document.createTextNode(`${i}`);
+    const bookAuthor = document.createElement("p");
+    bookAuthor.innerText = `Author: ${currentBook.author}`;
+    const bookPages = document.createElement("p");
+    bookPages.innerText = `Pages: ${currentBook.pages} pages`;
+    const bookRead = document.createElement("p");
+    bookRead.innerText = `${currentBook.read ? "Read" : "Not read"}`;
+    const bookCount = document.createElement("p");
+    bookCount.innerText = `${i + 1}`;
     bookCount.classList.add("count");
 
     bookCard.append(bookTitle);
@@ -49,11 +54,11 @@ function displayAllBooks() {
 
     libraryDiv.append(bookCard);
   }
-
-  addBookToLibrary("There Is No Antimemetics Division", "qntm", 288, false);
-  addBookToLibrary("Infinite Jest", "David Foster Wallace", 1104, false);
-  addBookToLibrary("The Nightingale", "Kristin Hannah", 448, false);
-  addBookToLibrary("Dog Man - A Graphic Novel", "Dav Pilkey", 240, false);
-
-  displayAllBooks();
 }
+
+addBookToLibrary("There Is No Antimemetics Division", "qntm", 288, false);
+addBookToLibrary("Infinite Jest", "David Foster Wallace", 1104, false);
+addBookToLibrary("The Nightingale", "Kristin Hannah", 448, false);
+addBookToLibrary("Dog Man - A Graphic Novel", "Dav Pilkey", 240, false);
+
+displayAllBooks();
